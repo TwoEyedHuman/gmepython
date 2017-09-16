c89c98c9761339495d51c2cf3feaa6c59d586a03
Brandon Locke <btl33@drexel.edu>
2017-09-16 09:42:22 -0400

    Removed the repeat function and added the die roll function

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import json
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
+from random import randint
+
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -14,10 +16,8 @@ def webhook():
 	data = request.get_json()
 	log('Recieved {}'.format(data))
 
-	# We don't want to reply to ourselves!
-	if data['name'] != 'MyBot':
-		msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-		send_message(msg)
+	if 'roll' in data['text'] and die in data['text']:
+		roll_dice(data)
 
 	return "ok", 200
 
@@ -34,3 +34,8 @@ def send_message(msg):
 def log(msg):
 	print(str(msg))
 	sys.stdout.flush()
+
+def roll_dice(data):
+	numbers = [int(s) for s in data['text'].split() if s.isdigit()]
+	result_dice = randint(1,numbers[0])
+	send_message("Your die landed on " + result_dice)
